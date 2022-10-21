d80268a16a9f3356c04bc455dfe5ed46b6a60a20
CelicaRayna <celica.raynna@gmail.com>
2022-10-21 20:21:11 -0300

    Renderizado o componente Title dentro do componente Missions

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -4,7 +4,9 @@ import Title from './Title';
 class Missions extends React.Component {
   render() {
     return (
-      <div data-testid="missions"><Title /></div>
+      <div data-testid="missions">
+        <Title headline="Missões" />
+      </div>
     );
   }
 }

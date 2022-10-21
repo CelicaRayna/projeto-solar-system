0bef87e2bd40c4aba7d3bd8511a4234cee0cb7af
CelicaRayna <celica.raynna@gmail.com>
2022-10-21 19:40:22 -0300

    Renderizada a lista com os planetas do Sistema Solar

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Title from './Title';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 class SolarSystem extends React.Component {
   render() {
@@ -7,7 +9,16 @@ class SolarSystem extends React.Component {
 
     return (
       <div data-testid="solar-system">
+
         <Title headline={ headline } />
+
+        {planets.map(({ name, image }) => (
+          <PlanetCard
+            key={ name }
+            planetName={ name }
+            planetImage={ image }
+          />
+        ))}
       </div>
     );
   }
